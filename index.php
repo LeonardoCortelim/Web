b8aab74e15d577be3b00d77da5b996176d47f199
leonardo <leo.cortelim@gmail.com>
2025-11-11 10:08:20 -0300

    Array

--- index.php
+++ index.php
@@ -6,52 +6,60 @@
     <title>Document</title>
 </head>
 <body>
-    Minha pagina html
+    Minha página HTML
     <?php
-        echo "<BR>"."Olá Mundão perdido!<BR>";
-        $nome="Leonardo Cortelim<BR>";
-        echo $nome."<BR>"; 
-
-        
+        echo "<br>Olá Mundão perdido!<br>";
+        $nome = "Leonardo Cortelim";
+        echo $nome . "<br>"; 
     ?>
 
     <h2>Exemplo tipo de dados</h2>
 
-<?php
-    $a=5;
-    $b=7;
-    echo "<p>".$a+$b."</p>";
-
+    <?php
+        $a = 5;
+        $b = 7;
+        echo "<p>" . ($a + $b) . "</p>";
     ?>
 
-    <h2>Operador ternario</h2>
+    <h2>Operador ternário</h2>
 
-<?php
-    $nota=3;
-    $situacao = $nota >=6 ? "aprovado" : "reprovado";
-    echo $situacao;
-?>
+    <?php
+        $nota = 3;
+        $situacao = $nota >= 6 ? "aprovado" : "reprovado";
+        echo $situacao;
+    ?>
 
-<h2>While</h2>
+    <h2>While</h2>
 
-<?php
-    $i =0;
-    while($i<50){
-        $i++;
-        echo "i=".$i. "-";
-    }
-?>
+    <?php
+        $i = 0;
+        while ($i < 50) {
+            $i++;
+            echo "i=" . $i . " - ";
+        }
+    ?>
 
-<h2>Array</h2>
+    <h2>Array</h2>
 
-<?php
-    $vetor = array(20,19,22,19,23);
-    for($posicao =0; $posicao <5; $posicao ++){
-    echo $vetor[$posicao] ."<br/>";
-}
-?>
+    <?php
+        $vetor = array(20, 19, 22, 19, 23);
+        for ($posicao = 0; $posicao < 5; $posicao++) {
+            echo $vetor[$posicao] . "<br>";
+        }
+    ?>
 
+    <h2>Array associativo</h2>
 
+    <?php
+        $vetor = array(
+            'nome' => 'Guilherme',
+            'sobrenome' => 'Silva',
+            'cpf' => '123.456.789-00'
+        );
 
+        echo 'Nome: ' . $vetor['nome'] . '<br>';
+        echo 'Sobrenome: ' . $vetor['sobrenome'] . '<br>';
+        echo 'CPF: ' . $vetor['cpf'];
+    ?>
 </body>
 </html>
